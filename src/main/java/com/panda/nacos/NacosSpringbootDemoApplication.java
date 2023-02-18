8b33e653808b8fca856c2dd37779de59d268933d
Yang <15620977808@163.com>
2023-02-18 23:49:09 +0800

    介绍如何使用 Nacos 来实现分布式环境下的配置管理

---
+++ src/main/java/com/panda/nacos/NacosSpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.panda.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosSpringbootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosSpringbootDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/panda/nacos/NacosSpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panda.nacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosSpringbootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
