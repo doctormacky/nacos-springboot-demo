5973498d561bcffcdccf619dd2840b8a198f6d05
Yang <15620977808@163.com>
2023-02-19 01:39:59 +0800

    介绍如何使用 Nacos 来实现分布式环境下的配置管理

--- src/main/java/com/panda/nacos/NacosSpringbootDemoApplication.java
+++ src/main/java/com/panda/nacos/NacosSpringbootDemoApplication.java
@@ -1,13 +1,11 @@
 package com.panda.nacos;
 
-import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 
 @EnableConfigurationProperties
-@NacosPropertySource(dataId = "db.properties", autoRefreshed = true)
 @SpringBootApplication
 public class NacosSpringbootDemoApplication {
 
